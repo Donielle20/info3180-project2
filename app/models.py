dcbbd166f2a5a019e20c198a5197d30c0da56b29
Donielle20 <donielle.hope@yahoo.com>
2023-04-14 16:46:49 -0500

    Started

--- app/models.py
+++ app/models.py
@@ -1,1 +1,49 @@
 # Add any model classes for Flask-SQLAlchemy here
+from . import db
+from werkzeug.security import generate_password_hash
+
+class Users(db.Model):
+    # You can use this to change the table name. The default convention is to use
+    # the class name. In this case a class name of UserProfile would create a
+    # user_profile (singular) table, but if we specify __tablename__ we can change it
+    # to `user_profiles` (plural) or some other name.
+    __tablename__ = 'Users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique = True)
+    password = db.Column(db.String(100), nullable = False)
+    firstname = db.Column(db.String(80))
+    lastname = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    location = db.Column(db.String(80))
+    biography = db.Column(db.String(80))
+    profile_photo= db.Column(db.String(80))
+    joined_on = db.Column(db.DateTime(), nullable = False)
+
+    def __init__(self, title, description, rooms, baths, price, type, location, filename):
+        self.title = title
+        self.description = description
+        self.rooms = rooms
+        self.baths = baths
+        self.price = price
+        self.type = type
+        self.location = location
+        self.filename = filename
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2 support
+        except NameError:
+            return str(self.id)  # python 3 support
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
